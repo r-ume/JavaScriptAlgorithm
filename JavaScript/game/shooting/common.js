2219f6eb1bc4b8ca6fdd295235858076e8c5ac3a
r-ume <baymax.blackandwhite@gmail.com>
2017-06-09 01:58:01 +0900

    initial commit of shooting game, which is for learnig JavaScript deeply

---
+++ JavaScript/game/shooting/common.js
@@ -0,0 +1,5 @@
+// Declaring a class 
+function Point(){
+	this.x = 0;
+	this.y = 0;
+}
---
+++ JavaScript/game/shooting/main.js
@@ -0,0 +1,13 @@
+// global variables
+
+var screenCanvas, info;
+var run = true;
+var fps = 1000 / 30;
+var mouse = new Point();
+
+
+// main 
+window.onload = function(){
+	screenCanvas = document.getElementById('screen');
+	
+}
