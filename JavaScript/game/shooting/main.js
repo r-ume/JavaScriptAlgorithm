45d1c7eaaf6138e714b047f3371467022350e5e3
r-ume <baymax.blackandwhite@gmail.com>
2017-06-09 12:14:39 +0900

    added mousemove and keydown functions

--- JavaScript/game/shooting/main.js
+++ JavaScript/game/shooting/main.js
@@ -1,4 +1,4 @@
-// global variables
+// global variables ----------------
 
 var screenCanvas, info;
 var run = true;
@@ -6,8 +6,41 @@ var fps = 1000 / 30;
 var mouse = new Point();
 
 
-// main 
+// - main ---------------------------
 window.onload = function(){
+	// initializing the screen
 	screenCanvas = document.getElementById('screen');
-	
+	screenCanvas.width = 256;
+	screenCanvas.height = 256;
+
+	// Registering events
+	screenCanvas.addEventListener('mousemove', mouseMove, true);
+	window.addEventListener('keydown', keyDown, true);
+
+	info = document.getElementById('info');
+
+	(function () {
+		// Reload html
+		info.innerHTML = mouse.x + ':' + mouse.y;
+
+		if(run){
+			setTimeout(arguments.callee, fps);
+		}
+
+	})();
+};
+
+// - event functions -------------------
+function mouseMove(event){
+	// Getting the position of mouse
+	mouse.x = event.clientX - screenCanvas.offsetLeft;
+	mouse.y = event.clientY - screenCanvas.offsetTop;
+}
+
+function keyDown(event){
+	// Getting keyocode
+	var ck = event.keyCode;
+
+	// if the keycode is 27, game stops.
+	if(ck === 27){ run = false };
 }
