93a516a93af99fa62a42e378d9e9c13c7e9c0489
r-ume <baymax.blackandwhite@gmail.com>
2017-06-09 21:40:06 +0900

    declared charactershot class!

--- JavaScript/game/shooting/scripts/character.js
+++ JavaScript/game/shooting/scripts/character.js
@@ -6,3 +6,31 @@ function Character(){
 Character.prototype.init = function(size){
 	this.size = size;
 }
+
+function CharacterShot(){
+	this.position = new Point();
+	this.size = 0;
+	this.speed = 0;
+	this.alive = false;
+}
+
+CharacterShot.prototype.set = function(p, size, speed){
+	// setting coordinates
+	this.position.x = p.x;
+	this.position.y = p.y;
+
+	// setting size and speed
+	this.size = size;
+	this.speed = speed;
+
+	// setting the status
+	this.alive = true;
+}
+
+CharacterShot.prototype.move = function(){
+	this.position.y -= this.speed;
+
+	if(this.position.y < -this.size){
+		this.alive = false;
+	}
+}
