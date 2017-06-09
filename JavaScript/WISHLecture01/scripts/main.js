a2cf227b5ea76cb4012a5292ad583b87d0e0d208
r-ume <baymax.blackandwhite@gmail.com>
2017-06-09 14:53:02 +0900

    change the name of variable

--- JavaScript/WISHLecture01/scripts/main.js
+++ JavaScript/WISHLecture01/scripts/main.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
-	var window_width   = $(window).width();
-	var main_width     = $(".main_container").width();
-	var long_container = $(".long_container");
-	var item           = $(".item");
-	var slide_number    = 0;
+	var window_width        = $(window).width();
+	var main_width          = $(".main_container").width();
+	var long_container      = $(".long_container");
+	var item                = $(".item");
+	var slide_number        = 0;
+	var photo_num           = $(".item").length;
+	var photo_displayed     = 3;
+	var photo_not_displayed = photo_num - photo_displayed; 
 
 	// editting css dynamtically
 	long_container.css("width", main_width * 3.05);
 	item.css("width", long_container.width() / 9);
 
 	$(".see_next").click(function(){
-		if(slide_number < 6){
+		if(slide_number < photo_not_displayed){
 			slide_number++;
 
-			if(slide_number == 6){
+			if(slide_number == photo_not_displayed){
 				$(".see_next").animate({
 					opacity: 0
 				});
@@ -56,7 +59,7 @@ $(document).ready(function(){
 				});
 			}
 
-			if(slide_number == 6){
+			if(slide_number == photo_not_displayed){
 				$(".see_next").animate({
 					opacity: 0
 				});
