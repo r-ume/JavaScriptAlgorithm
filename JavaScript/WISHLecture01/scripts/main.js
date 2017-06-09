bef5f63a45b8ec31e335dd1dd570d761d0fe0cd4
r-ume <baymax.blackandwhite@gmail.com>
2017-06-09 14:34:03 +0900

    photo can be moved to left in the gallery

--- JavaScript/WISHLecture01/scripts/main.js
+++ JavaScript/WISHLecture01/scripts/main.js
@@ -3,9 +3,44 @@ $(document).ready(function(){
 	var main_width     = $(".main_container").width();
 	var long_container = $(".long_container");
 	var item           = $(".item");
-	var side_number    = 0;
+	var slide_number    = 0;
 
+	// editting css dynamtically
 	long_container.css("width", main_width * 3.05);
 	item.css("width", long_container.width() / 9);
+
+	$(".see_next").click(function(){
+		if(slide_number < 6){
+			slide_number++;
+
+			if(slide_number == 6){
+				$(".see_next").animate({
+					opacity: 0
+				});
+			}else {
+				$(".see_next").animate({
+					opacity: 1
+				});
+			}
+
+			if(slide_number == 0){
+				$(".see_previous").animate({
+					opacity: 0
+				});
+			}else{
+				$(".see_previous").animate({
+					opacity: 1
+				});
+			}
+
+			var item_width   = $(".item").width();
+			var pixels_moved = item_width * slide_number;
+		
+			long_container.animate({
+				marginLeft: -pixels_moved
+			});
+		}
+	});
+
 
 });
