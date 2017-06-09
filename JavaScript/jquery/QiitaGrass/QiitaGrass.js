f3b66dd9e679f327081e23f51e9b51add40dfd57
r-ume <baymax.blackandwhite@gmail.com>
2017-06-09 15:56:18 +0900

    succeeded in getting response from qiita api

--- JavaScript/jquery/QiitaGrass/QiitaGrass.js
+++ JavaScript/jquery/QiitaGrass/QiitaGrass.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	// 
+	// getting user_id from the form
 	$("#form").on("submit", function(event){
 		$("#btn-submit").prop("disabled", true);
 		var user_id = $("#user_id").val();
@@ -10,12 +10,15 @@ $(function(){
 			url: "http://qiita.com/api/v2/users/" + user_id + "/items?page=1&per_page=100",
 			success: onSucceed
 		});
-
 		// If the event being executed can be stopped, preventDefault cancels the event.
 		event.preventDefault();
 	});
 
+	// date format
+	var format = d3.time.format("%Y-%m-%d");
+
 	function onSucceed(data){
+		console.log(data);
 	};
 
 });
