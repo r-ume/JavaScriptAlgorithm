2f4b3ab555961e1afa270e9f910c256daeb03bb3
r-ume <baymax.blackandwhite@gmail.com>
2017-06-10 14:02:35 +0900

    enemies can make shots

--- JavaScript/game/shooting/scripts/common.js
+++ JavaScript/game/shooting/scripts/common.js
@@ -1,5 +1,28 @@
-// Declaring a class 
+// Declaring a class
+// - point --------------------------------------------------------------------
 function Point(){
 	this.x = 0;
 	this.y = 0;
 }
+
+Point.prototype.distance = function(p){
+	var q = new Point();
+	q.x = p.x - this.x;
+	q.y = p.y - this.y;
+	return q;
+};
+
+Point.prototype.length = function(){
+	return Math.sqrt(this.x * this.x + this.y * this.y);
+};
+
+Point.prototype.normalize = function(){
+	var i = this.length();
+	if(i > 0){
+		var j = 1 / i;
+		this.x *= j;
+		this.y *= j;
+	}
+};
+
+
--- JavaScript/game/shooting/scripts/main.js
+++ JavaScript/game/shooting/scripts/main.js
@@ -1,168 +1,202 @@
-// - global variables ----------------
+// - global variables -------------------------------------------------------------------
 var screenCanvas, info;
 var run = true;
 var fps = 1000 / 30;
 var mouse = new Point();
 var ctx;
-var permit_fire = false;
+var fire = false;
 var counter = 0;
 
-// - constant variables -------
+// - constant variables --------------------------------------------------------------------
 var CHARACTER_COLOR = 'rgba(0, 0, 255, 0.75)';
 var CHARACTER_SHOT_COLOR = 'rgba(0, 255, 0, 0.75)';
-var ENEMY_COLOR = 'rgba(255, 0, 0, 0.75)'
 var CHARACTER_SHOT_MAX_COUNT = 10;
+var ENEMY_COLOR = 'rgba(255, 0, 0, 0.75)';
 var ENEMY_MAX_COUNT = 10;
+var ENEMY_SHOT_COLOR = 'rgba(255, 0, 255, 0.75)';
+var ENEMY_SHOT_MAX_COUNT = 100;
 
-// - main ---------------------------
+// - main ---------------------------------------------------------------------
 window.onload = function(){
 	var i, j;
 	var p = new Point();
-
-	// initializing the screen
+	
 	screenCanvas = document.getElementById('screen');
 	screenCanvas.width = 256;
 	screenCanvas.height = 256;
-
-	// using canvas2d
+	
 	ctx = screenCanvas.getContext('2d');
-
-	// Registering events
+	
 	screenCanvas.addEventListener('mousemove', mouseMove, true);
 	screenCanvas.addEventListener('mousedown', mouseDown, true);
 	window.addEventListener('keydown', keyDown, true);
-
+	
 	info = document.getElementById('info');
-
-	// making an instance of Character class
+	
 	var character = new Character();
 	character.init(10);
-
-	var enemy = new Array(ENEMY_MAX_COUNT);
-	for(var i = 0; i < ENEMY_MAX_COUNT; i++){
-		enemy[i] = new Enemy();
- 	}
-
-	// making instances of CharacterShot class
+	
 	var characterShot = new Array(CHARACTER_SHOT_MAX_COUNT);
-	for(var i = 0; i < CHARACTER_SHOT_MAX_COUNT; i++){
+	for(i = 0; i < CHARACTER_SHOT_MAX_COUNT; i++){
 		characterShot[i] = new CharacterShot();
 	}
-
-	(function () {
+	
+	var enemy = new Array(ENEMY_MAX_COUNT);
+	for(i = 0; i < ENEMY_MAX_COUNT; i++){
+		enemy[i] = new Enemy();
+	}
+	
+	var enemyShot = new Array(ENEMY_SHOT_MAX_COUNT);
+	for(i = 0; i < ENEMY_SHOT_MAX_COUNT; i++){
+		enemyShot[i] = new EnemyShot();
+	}
+	
+	(function(){
 		counter++;
-
-		// Reload html
-		info.innerHTML = mouse.x + ':' + mouse.y;
-
+		
+		info.innerHTML = mouse.x + ' : ' + mouse.y;
+	
 		ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
-		// declaring a path is important when it comes to canvas2d
-		// need to set the properties(position, color, and etc)
+		
+		// - character -----
 		ctx.beginPath();
-
-		// setting the position of the player.
+		
 		character.position.x = mouse.x;
 		character.position.y = mouse.y;
-
-		// setting character's color
-		ctx.fillStyle = CHARACTER_COLOR;
-
-		// setting the path
-		ctx.arc(character.position.x, character.position.y, character.size, 0, Math.PI * 2, false);
 		
-		// draw the player
+		ctx.arc(
+			character.position.x,
+			character.position.y,
+			character.size,
+			0, Math.PI * 2, false
+		);
+		
+		ctx.fillStyle = CHARACTER_COLOR;
+		
 		ctx.fill();
-
-		if(permit_fire){
-			for(var i = 0; i < CHARACTER_SHOT_MAX_COUNT; i++){
+		
+		if(fire){
+			for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
 				if(!characterShot[i].alive){
-					// start position is 
 					characterShot[i].set(character.position, 3, 5);
-
+					
 					break;
 				}
 			}
-
-			permit_fire = false;
+			fire = false;
 		}
-
+		
+		// - character shot ---- 
 		ctx.beginPath();
-
+		
 		for(i = 0; i < CHARACTER_SHOT_MAX_COUNT; i++){
+			
 			if(characterShot[i].alive){
-				characterShot[i].move();
-
+ 				characterShot[i].move();
+				
 				ctx.arc(
 					characterShot[i].position.x,
 					characterShot[i].position.y,
 					characterShot[i].size,
 					0, Math.PI * 2, false
 				);
-
+			
 				ctx.closePath();
 			}
 		}
-
+		
 		ctx.fillStyle = CHARACTER_SHOT_COLOR;
+		
 		ctx.fill();
-
+		
 		if(counter % 100 === 0){
-			for(var i = 0; i < ENEMY_MAX_COUNT; i++){
+			for(i = 0; i < ENEMY_MAX_COUNT; i++){
 				if(!enemy[i].alive){
-					type = (counter % 200) / 100;
-
+					j = (counter % 200) / 100;
+					
 					var enemySize = 15;
-					p.x = -enemySize + (screenCanvas.width + enemySize * 2) * type
+					p.x = -enemySize + (screenCanvas.width + enemySize * 2) * j
 					p.y = screenCanvas.height / 2;
-
-					enemy[i].set(p, enemySize, type);
-
+					
+					enemy[i].set(p, enemySize, j);
+					
 					break;
 				}
 			}
 		}
-
+		
 		ctx.beginPath();
-
-		for(var i = 0; i < ENEMY_MAX_COUNT; i++){
+		
+		for(i = 0; i < ENEMY_MAX_COUNT; i++){
 			if(enemy[i].alive){
 				enemy[i].move();
-
+				
 				ctx.arc(
 					enemy[i].position.x,
 					enemy[i].position.y,
 					enemy[i].size,
 					0, Math.PI * 2, false
 				);
-
+				
+				if(enemy[i].param % 30 === 0){
+					for(j = 0; j < ENEMY_SHOT_MAX_COUNT; j++){
+						if(!enemyShot[j].alive){
+							p = enemy[i].position.distance(character.position);
+							p.normalize();
+							enemyShot[j].set(enemy[i].position, p, 5, 5);
+							
+							break;
+						}
+					}
+				}
+				
 				ctx.closePath();
 			}
 		}
-
+		
 		ctx.fillStyle = ENEMY_COLOR;
-
-		ctx.fill()
-
+		
+		ctx.fill();
+		
+		ctx.beginPath();
+		
+		for(i = 0; i < ENEMY_SHOT_MAX_COUNT; i++){
+			if(enemyShot[i].alive){
+				enemyShot[i].move();
+				
+				ctx.arc(
+					enemyShot[i].position.x,
+					enemyShot[i].position.y,
+					enemyShot[i].size,
+					0, Math.PI * 2, false
+				);
+				
+				ctx.closePath();
+			}
+		}
+		
+		ctx.fillStyle = ENEMY_SHOT_COLOR;
+		
+		ctx.fill();
+		
 		if(run){setTimeout(arguments.callee, fps);}
-
 	})();
 };
 
-// - event functions -------------------
+
+// - event --------------------------------------------------------------------
 function mouseMove(event){
-	// Getting the position of mouse
 	mouse.x = event.clientX - screenCanvas.offsetLeft;
 	mouse.y = event.clientY - screenCanvas.offsetTop;
 }
 
 function mouseDown(event){
-	permit_fire = true;
+	fire = true;
 }
 
 function keyDown(event){
-	// Getting keyocode
 	var ck = event.keyCode;
-
-	// if the keycode is 27, game stops.
-	if(ck === 27){ run = false };
+	
+	if(ck === 27){run = false;}
 }
+
