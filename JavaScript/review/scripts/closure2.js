14fc7688b8891ffe01261a116491637f95958d1f
r-ume <baymax.blackandwhite@gmail.com>
2017-06-15 19:20:01 +0900

    added closure3.js

--- JavaScript/review/scripts/closure2.js
+++ JavaScript/review/scripts/closure2.js
@@ -9,12 +9,18 @@ function celebrity_name(first_name){
 }
 
 /*
- * function last_name(the_last_name){
-		return name_intro + first_name + the_last_name;
+ *  At this juncture, the celebrity_name outer function has returned.  
+ *  result - 
+    function last_name(the_last_name){
+		  return name_intro + first_name + the_last_name;
+ 	  }
  */
+
 console.log(celebrity_name("Michel"));
 
 var mjName = celebrity_name("Michel");
 
+// The closure (lastName) is called here after the outer function has returned above.
+// Yet, the closure still has access to the outer function's variables and parameter
 // This celebrity is MichelJackson
 console.log(mjName("Jackson"));
---
+++ JavaScript/review/scripts/closure3.js
@@ -0,0 +1,28 @@
+function celebrityID(){
+	var celebrityID = 999;
+
+
+	// object
+	// We are returning an object with some inner functions
+	// All the inner functions have access to the outer function's variables
+	return{
+		getID: function(){
+
+			// This inner function will return the UPDATED celebrityID variable
+			// It will return the current value of celebrityID, even after the changeTheID function changes it
+			return celebrityID;
+		},
+		setID: function(theNewID){
+			// This inner function will change the outer's function variable anytime.
+			celebrityID = theNewID;
+		}
+	}
+}
+
+// At this juncture, the celebrityID outer function has returned.
+var mjID = celebrityID();
+console.log(mjID); // return Object {getID: function, setID: function }
+
+mjID.getID(); // 999
+mjID.setID(567); // changes the outer function's variable.
+mjID.getID(); // 567
