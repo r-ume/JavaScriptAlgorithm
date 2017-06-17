4d3fea6b5739e688c0d917b774284875af9d8539
r-ume <baymax.blackandwhite@gmail.com>
2017-06-17 18:35:05 +0900

    the app stops when a word gets down to the bottom

--- JavaScript/WISHLecture02/TypingGame.js
+++ JavaScript/WISHLecture02/TypingGame.js
@@ -6,121 +6,127 @@ var PIECE_SIZE      = 80;
 var PIECE_SIZE_HALF = PIECE_SIZE / 2;
 
 phina.define("MainScene", {
-	superClass: 'DisplayScene',
+  superClass: 'DisplayScene',
 
-	init: function(){
-		this.superInit({
-			width:  SCREEN_WIDTH,
-			height: SCREEN_HEIGHT
-		});
+  init: function(){
+    this.superInit({
+      width:  SCREEN_WIDTH,
+      height: SCREEN_HEIGHT
+    });
 
-		this.fromJSON({
-			children: {
-				wordGroup: {
-					className: 'DisplayElement'
-				},
-				scoreLabel: {
-					className: 'Label',
-					x: this.gridX.span(15),
-					y: this.gridX.span(1),
-					align: 'right'
-				}
-			}
-		});
+    this.fromJSON({
+      children: {
+        wordGroup: {
+          className: 'DisplayElement'
+        },
+        scoreLabel: {
+          className: 'Label',
+          x: this.gridX.span(15),
+          y: this.gridX.span(1),
+          align: 'right'
+        }
+      }
+    });
 
-		this.score = 0;
-		this.scoreLabel.text = this.score + '';
-	},
+    this.score = 0;
+    this.scoreLabel.text = this.score + '';
+  },
 
-	onkeydown: function(event){
-		var input_character = String.fromCharCode(event.keyCode);
-		var wordGroup = this.wordGroup;
-		var result = wordGroup.children.some(function(word){
-			if(word.enable && word.text == input_character){
-				word.disappear();
-				return true;
-			}
+  onkeydown: function(event){
+    var input_character = String.fromCharCode(event.keyCode);
+    var wordGroup = this.wordGroup;
+    var result = wordGroup.children.some(function(word){
+      if(word.enable && word.text == input_character){
+        word.disappear();
+        return true;
+      }
 
-			return false;
-		});
+      return false;
+    });
 
-		if(result){
-			this.score += 1;
-			this.scoreLabel.text = this.score + '';
-		}
+    if(result){
+      this.score += 1;
+      this.scoreLabel.text = this.score + '';
+    }
 
-		if(event.keyCode === 32){
-			this.app.stop();
-		}
-	},
+    if(event.keyCode === 32){
+      this.app.stop();
+    }
+  },
 
-	update: function(app){
-		if(app.frame % 16 === 0){
-			this.createWord();
-		}
-	},
+  update: function(app){
+    if(app.frame % 16 === 0){
+      this.createWord();
+    }
+  },
 
-	createWord: function(){
-		var ascii = [
-			48, 49,
-		 	50, 51, 52, 53, 54, 55, 56, 57,
-			65, 66, 67, 68, 69, 
-			70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
-			80, 81, 82, 83, 84, 85, 86, 87, 88, 89
-		];
+  createWord: function(){
+    var ascii = [
+      48, 49,
+      50, 51, 52, 53, 54, 55, 56, 57,
+      65, 66, 67, 68, 69, 
+      70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
+      80, 81, 82, 83, 84, 85, 86, 87, 88, 89
+    ];
 
-		var character = String.fromCharCode(ascii.pickup());
-		var word = Word(character).addChildTo(this.wordGroup);
-		word.x = Math.randint(PIECE_SIZE_HALF, this.gridX.width - PIECE_SIZE_HALF);
-		word.y = -100;
+    var character = String.fromCharCode(ascii.pickup());
+    var word = Word(character).addChildTo(this.wordGroup);
+    word.x = Math.randint(PIECE_SIZE_HALF, this.gridX.width - PIECE_SIZE_HALF);
+    word.y = -100;
 
-		return word;
-	}
+    word.onattack = function() {
+      this.exit({
+        score: this.score,
+      });
+    }.bind(this);
+
+    return word;
+  }
 });
 
 phina.define('Word', {
-	superClass: 'Button',
+  superClass: 'Button',
 
-	init: function(word){
-		this.superInit({
-			width:  PIECE_SIZE,
-			height: PIECE_SIZE,
-			text:   word
-		});
+  init: function(word){
+    this.superInit({
+      width:  PIECE_SIZE,
+      height: PIECE_SIZE,
+      text:   word
+    });
 
-		this.enable = true;
-	},
+    this.enable = true;
+  },
 
-	update: function(){
-		this.y += 8;
+  update: function(){
+    this.y += 8;
 
-		if(this.y > 960){
-			this.flare('attack');
-			this.remove();
-		}
-	},
+    if(this.y > 960){
+      this.flare('attack');
+      this.remove();
+    }
+  },
 
-	disappear: function(){
-		this.enable = false;
-		this.tweener
-				.to({
-					scaleX: 2, 
-					scaleY: 2,
-					alpha: 0,
-				}, 250)
-				.call(function(){
-					this.target.remove();
-				});
-	}
+  disappear: function(){
+    this.enable = false;
+    this.tweener
+        .to({
+          scaleX: 2, 
+          scaleY: 2,
+          alpha: 0,
+        }, 250)
+        .call(function(){
+          this.target.remove();
+        });
+  }
 });
 
 phina.main(function() {
-	var app = GameApp({
-		title: 'Typing Game',
-		width: SCREEN_WIDTH,
-		height: SCREEN_HEIGHT,
-		startLabel: location.search.substr(1).toObject().scene || 'title',
-	});
+  var app = GameApp({
+    title: 'Typing Game',
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    startLabel: location.search.substr(1).toObject().scene || 'title',
+  });
 
-	app.run();
+  app.run();
 });
