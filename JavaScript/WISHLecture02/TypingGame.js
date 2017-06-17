abbb424cb7489b5c53b251bb22bc4827efc3b4e1
r-ume <baymax.blackandwhite@gmail.com>
2017-06-17 17:03:20 +0900

    added main function of phina.js

--- JavaScript/WISHLecture02/TypingGame.js
+++ JavaScript/WISHLecture02/TypingGame.js
@@ -5,3 +5,14 @@ var SCREEN_WIDTH    = 640;
 var SCREEN_HEIGHT   = 960;
 var PIECE_SIZE      = 80;
 var PIECE_SIZE_HALF = PIECE_SIZE / 2;
+
+phina.main(function() {
+	var app = GameApp({
+		title: 'Typing Game',
+		width: SCREEN_WIDTH,
+		height: SCREEN_HEIGHT,
+		startLabel: location.search.substr(1).toObject().scene || 'title',
+	});
+
+	app.run();
+});
