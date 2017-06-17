b9bb241b20f30dc6a34927fb4fe041fa1a3273cd
r-ume <baymax.blackandwhite@gmail.com>
2017-06-17 17:09:21 +0900

    succeeded in displaying text label

--- JavaScript/WISHLecture02/TypingGame.js
+++ JavaScript/WISHLecture02/TypingGame.js
@@ -6,6 +6,35 @@ var SCREEN_HEIGHT   = 960;
 var PIECE_SIZE      = 80;
 var PIECE_SIZE_HALF = PIECE_SIZE / 2;
 
+phina.define("MainScene", {
+	superClass: 'DisplayScene',
+
+	init: function(){
+		this.superInit({
+			width:  SCREEN_WIDTH,
+			height: SCREEN_HEIGHT
+		});
+
+		this.fromJSON({
+			children: {
+				wordGroup: {
+					className: 'DisplayElement'
+				},
+				scoreLabel: {
+					className: 'Label',
+					text: '999',
+					x: this.gridX.span(15),
+					y: this.gridX.span(1),
+					align: 'right'
+				}
+			}
+		});
+
+		this.score = 0;
+		this.scoreLabel.text = this.score + '';
+	}
+})
+
 phina.main(function() {
 	var app = GameApp({
 		title: 'Typing Game',
