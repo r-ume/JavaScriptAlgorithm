12c98a3a0512d09859c52271f122d3c4330e65fa
r-ume <baymax.blackandwhite@gmail.com>
2017-06-18 11:40:51 +0900

    added functions that add and remove comma

--- JavaScript/jquery/calculation/Calculation.js
+++ JavaScript/jquery/calculation/Calculation.js
@@ -9,8 +9,33 @@ $(function(){
   var optionsPrice = 0;
   var basePrice = priceBase;
 
-  $(".priceTotal").text(priceTotal);
+  $(".priceTotal").text(addComma(priceTotal));
 
-  
 
 });
+
+// A function that adds comma to yen
+function addComma(str){
+  // 正規表現
+
+  // g - グローバルオプション 複数回マッチする
+  // ^ - 入力の先頭にはマッチする
+  // 例えば /^A/ は "an A" の 'A' にはマッチするが、"An E" の 'A' にはマッチしない。
+
+  // \d - 数字
+  // + 直前の文字の連続でもマッチするようにする。
+  // -（ハイフンがわからない) 
+
+  // カンマがあるところを先になくす。
+  var num = new String(str).replace(/,/g, "");
+  var yenPattern = /^(-?\d+)(\d{3})/;
+  var numWithCommna = num.replace(yenPattern, "$1,$2");
+  return numWithCommna;
+}
+
+// A function that removes a comma from yen
+function removeComma(str){
+  var num = new String(str).replace(/,/g, "");
+  num = Number(num);
+  return num;
+}
