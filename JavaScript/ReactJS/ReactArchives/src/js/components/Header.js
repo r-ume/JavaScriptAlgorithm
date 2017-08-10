db3a0c340aa433521bea600920db7d5f19a6688c
r-ume <baymax.blackandwhite@gmail.com>
2017-08-10 14:47:15 +0900

    added handleChange and changeTitle functions to let the title change as input changes

--- JavaScript/ReactJS/ReactArchives/src/js/components/Header.js
+++ JavaScript/ReactJS/ReactArchives/src/js/components/Header.js
@@ -3,10 +3,18 @@ import React from "react";
 import Title from "./Header/Title";
 
 export default class Header extends React.Component{
-	render(){
-		return (
-			<Title />
-		);
-	}
-	
+
+  handleChange(event){
+    const title = event.target.value;
+    this.props.changeTitle(title);
+  }
+
+  render(){
+    return (
+      <div>
+        <Title title={this.props.title}/>
+        <input onChange={this.handleChange.bind(this)} />
+      </div>
+    );
+  }
 }
--- JavaScript/ReactJS/ReactArchives/src/js/components/Layout.js
+++ JavaScript/ReactJS/ReactArchives/src/js/components/Layout.js
@@ -5,12 +5,25 @@ import Footer from "./Footer";
 
 export default class Layout extends React.Component{
 
+  constructor(){
+    super();
+    this.state = {
+      title: "Welcome",
+    };
+  }
+
+  changeTitle(title){
+    // same as this.setState({title: title})
+    this.setState({title});
+  }
+
+  // When you would like to pass functions, you should bind it to this.
   render() {
-  	return (
-  		<div>
-  			<Header />
-  			<Footer />
-			</div>
-		);
+    return (
+      <div>
+        <Header changeTitle = {this.changeTitle.bind(this)} title = {this.state.title} />
+        <Footer />
+      </div>
+    );
   }
 }
