6c0b8d28bf0c195855bf0fee119713f69379943d
r-ume <baymax.blackandwhite@gmail.com>
2017-08-12 18:12:39 +0900

    added ways of using data binding

--- JavaScript/VueJS/VBlog/app.js
+++ JavaScript/VueJS/VBlog/app.js
@@ -1,7 +1,15 @@
 new Vue({
   el: '#vue-app',
   data: {
-  	name: 'baymax'
+    name: 'baymax',
+    job: 'robot',
+    website: 'https://www.yahoo.co.jp/',
+    websiteTag: '<a href = "http://www.thenetninja.co.uk">The Net Ninja Website</a>'
+  },
+  methods: {
+    greet: function(time){
+      return 'Good ' + time + ' ' + this.name;
+    }
   }
 });
 
