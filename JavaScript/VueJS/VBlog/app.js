1c4b8f5d88920319f2190d701fe9e2dd9d88acd5
r-ume <baymax.blackandwhite@gmail.com>
2017-08-12 18:30:03 +0900

    added ways of using functions

--- JavaScript/VueJS/VBlog/app.js
+++ JavaScript/VueJS/VBlog/app.js
@@ -4,11 +4,24 @@ new Vue({
     name: 'baymax',
     job: 'robot',
     website: 'https://www.yahoo.co.jp/',
-    websiteTag: '<a href = "http://www.thenetninja.co.uk">The Net Ninja Website</a>'
+    websiteTag: '<a href = "http://www.thenetninja.co.uk">The Net Ninja Website</a>',
+    age: 25,
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function(time){
       return 'Good ' + time + ' ' + this.name;
+    },
+    add: function(increment){
+      this.age += increment;
+    },
+    subtract: function(decrement){
+      this.age -= decrement;
+    },
+    updateXY: function(event){
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
