c615f3697936d36bdec6418b1f317ed85c971858
r-ume <baymax.blackandwhite@gmail.com>
2017-08-16 14:40:31 +0900

    got to know get and post api with vue js

--- JavaScript/VueJS/VueBlog/src/main.js
+++ JavaScript/VueJS/VueBlog/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
 // Setting a component globally.
 // Vue.component('ninja', Ninjas);
+
+Vue.use(VueResource)
 
 new Vue({
   el: '#app',
