ae456b6f6a3a85684c48349dc9f7e9b4873fea57
r-ume <baymax.blackandwhite@gmail.com>
2017-08-19 11:38:32 +0900

    added the way of passing params id

--- JavaScript/VueJS/VueBlog/src/components/showBlogs.vue
+++ JavaScript/VueJS/VueBlog/src/components/showBlogs.vue
@@ -1,18 +1,25 @@
 <template>
-  <div id = 'show-blogs'>
+  <!-- Be careful that wide should be in 'wide" -->
+  <div v-theme:column = "'narrow'" id = 'show-blogs'>
     <h1>All Blog Articles</h1>
-    <div v-for = 'blog in blogs' class = 'single-blog'>
-      <h2>{{ blog.title }}</h2>
-      <article>{{ blog.body }}</article>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div v-for = 'blog in filteredBlogs' class = 'single-blog'>
+      <router-link v-bind:to = "'/blog/' + blog.id">
+        <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+      </router-link>
+      <article>{{ blog.body | spinnet }}</article>
     </div>
   </div>
 </template>
 
 <script>
+import searchMixin from '../mixins/searchMixin';
+
 export default {
   data(){
     return{
-      blogs: []
+      blogs: [],
+      search: ''
     }
   },
   methods: {
@@ -22,7 +29,29 @@ export default {
     this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
       this.blogs = data.body.slice(0, 10);
     })
-  }
+  },
+  computed: {
+    // filteredBlogs: function(){
+    //   return this.blogs.filter((blog) => {
+    //     return blog.title.match(this.search);
+    //   })
+    // }
+  },
+  // local registrations
+  filters: {
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  },
+  // local registrations
+  directives: {
+    'rainbow': {
+      bind(el, binding, vnode){
+       el.style.color = '#' + Math.random().toString().slice(2, 8);
+      }
+    }
+  },
+  mixins: [searchMixin]
 }
 </script>
 
