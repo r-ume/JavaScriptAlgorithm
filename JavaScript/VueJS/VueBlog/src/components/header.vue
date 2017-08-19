d5100eea3f74b38a5c5ce73991444da38de9e0aa
r-ume <baymax.blackandwhite@gmail.com>
2017-08-19 11:34:18 +0900

    added vue-routing in header.vue

--- JavaScript/VueJS/VueBlog/src/components/header.vue
+++ JavaScript/VueJS/VueBlog/src/components/header.vue
@@ -1,32 +1,43 @@
 <template>
-  <header>
-    <h1>{{ title }}</h1>
-  </header>
+  <nav>
+    <ul>
+      <li><router-link to = '/' exact>Blog</router-link></li>
+      <li><router-link to = '/add' exact>Add a New Blog</router-link></li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-export default {
-  props: {
-    title: {
-      type: String
-    }
-  },
-  data() {
-    return {
-      // title: 'Vue Ninjas'
-    }
-  }
-}
+export default {}
 </script>
 
 <style scoped>
-header{
-  background: lightgreen;
-  padding: 10px;
+ul{
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
 }
 
-h1{
-  color: #222;
-  text-align: center;
+li{
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a{
+  color: #fff;
+  text-decoration: none; 
+  padding: 6px 8px;
+  border-radius: 10px;
+}
+
+nav{
+  background: #444;
+  padding: 14px 0;
+  margin-bottom: 40px;
+}
+
+.router-link-active {
+  background: #eee;
+  color: #444; 
 }
 </style>
