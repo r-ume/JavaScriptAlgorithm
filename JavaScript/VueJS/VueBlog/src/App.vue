690d512730b06eeafa6a9a2caa9458d1d4a005f6
r-ume <baymax.blackandwhite@gmail.com>
2017-08-19 11:38:08 +0900

    added header and showBlogs.vue

--- JavaScript/VueJS/VueBlog/src/App.vue
+++ JavaScript/VueJS/VueBlog/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
-  <show-blogs></show-blogs>
+    <app-header></app-header>
+    <router-view></router-view>
+    <!-- <show-blogs></show-blogs>
+    <list-blogs></list-blogs> -->
   </div>
 </template>
 
 <script>
-  import addBlog from './components/addBlog.vue';
+  import addBlog   from './components/addBlog.vue';
   import showBlogs from './components/showBlogs.vue';
+  import listBlogs from './components/listBlogs.vue';
+  import header    from './components/header.vue';
 
   export default {
     components: {
       'add-blog': addBlog,
-      'show-blogs': showBlogs
+      'show-blogs': showBlogs,
+      'list-blogs': listBlogs,
+      'app-header': header
     },
     data() {
       return {
