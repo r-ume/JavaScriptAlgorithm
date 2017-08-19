fe5203b1576e0624ea71c3e56b4d3a47a93c0e2d
r-ume <baymax.blackandwhite@gmail.com>
2017-08-19 11:36:36 +0900

    registering functions locally

---
+++ JavaScript/VueJS/VueBlog/src/components/listBlogs.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- Be careful that wide should be in 'wide" -->
+  <div v-theme:column = "'narrow'" id = 'show-blogs'>
+    <h1>List Blog Titles</h1>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div v-for = 'blog in filteredBlogs' class = 'single-blog'>
+      <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import searchMixin from '../mixins/searchMixin';
+
+export default {
+  data(){
+    return{
+      blogs: [],
+      search: ''
+    }
+  },
+  methods: {
+
+  },
+  created(){
+    this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+      this.blogs = data.body.slice(0, 10);
+    })
+  },
+  computed: {
+    filteredBlogs: function(){
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      })
+    }
+  },
+  // local registrations
+  filters: {
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  },
+  // local registrations
+  directives: {
+    'rainbow': {
+      bind(el, binding, vnode){
+       el.style.color = '#' + Math.random().toString().slice(2, 8);
+      }
+    }
+  }, 
+  mixins: [searchMixin]
+}
+</script>
+
+<style>
+#show-blogs{
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.single-blog{
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
--- JavaScript/VueJS/VueBlog/src/main.js
+++ JavaScript/VueJS/VueBlog/src/main.js
@@ -1,13 +1,53 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue         from 'vue'
+import App         from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter   from 'vue-router'
+import Routes      from './routes'
 
 // Setting a component globally.
 // Vue.component('ninja', Ninjas);
 
-Vue.use(VueResource)
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes,
+  // ページのリロード無しに URL 遷移を実現する
+  mode: 'history'
+});
+
+// Custom directives
+// Vue.directive('rainbow', {
+//   bind(el, binding, vnode){
+//     el.style.color = '#' + Math.random().toString().slice(2, 8);
+//   }
+// });	
+
+Vue.directive('theme', {
+  bind(el, binding, vnode){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = '1200px';  
+    } else if (binding.value == 'narrow'){
+      el.style.maxWidth = '560px';
+    }
+    if(binding.arg == 'column'){
+      el.sytle.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+})
+
+// Filters
+// Vue.filter('to-uppercase', function(value){
+//   return value.toUpperCase();
+// });
+
+Vue.filter('spinnet', function(value){
+  return value.slice(0, 100) + '...';
+});
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
