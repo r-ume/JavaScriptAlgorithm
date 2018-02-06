9cea73cf3e74a6efd83f11e2b8cca15f34cf1f60
r-ume <baymax.blackandwhite@gmail.com>
2018-02-06 14:35:29 +0900

    added immediatelyInvokedFunctions.js

---
+++ NodeJs/review/immediatelyInvokedFunctions.js
@@ -0,0 +1,53 @@
+var result = (function (param1, param2) {
+    return param1 + param2;
+}(1, 2));
+
+console.log(result); //3が出力される。
+
+var objectCounter = {
+  //privateにしたいプロパティ
+  count: 0,
+
+  //加算メソッド
+  increment: function () {
+    this.count += 1;
+    console.log(this.count);
+  },
+
+  //減算メソッド
+  decrement: function () {
+    this.count -= 1;
+    console.log(this.count);
+  }
+};
+
+var immediatelyInvokedCounter = (function () {
+  //プライベートにしたいプロパティ
+  var count = 0;
+
+  return {
+    //加算メソッド
+    increment: function () {
+      count += 1;
+      console.log(count);
+    },
+
+    //減算メソッド
+    decrement: function () {
+      count -= 1;
+      console.log(count);
+    }
+  };
+}());
+ 
+objectCounter.increment(); //1が出力される
+objectCounter.increment(); //2が出力される
+objectCounter.decrement(); //1が出力される
+ 
+console.log(objectCounter.count);
+ 
+immediatelyInvokedCounter.increment(); //1が出力される
+immediatelyInvokedCounter.increment(); //2が出力される
+immediatelyInvokedCounter.decrement(); //1が出力される
+ 
+console.log(immediatelyInvokedCounter.count); //※countプロパティを直接参照／書き換えが出来てしまう
