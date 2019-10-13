89ec08c4d04a4861d4b21b94ee42233e6822184b
romukey <baymax.blackandwhite@gmail.com>
2019-10-13 17:27:36 +0900

    move all scripts in scripts directory

---
+++ scripts/condition.js
@@ -0,0 +1,15 @@
+const hoge = () => {
+  console.log('hoge')
+  return undefined
+}
+
+const fuga = () => {
+console.log('fuga')
+	return { w: '122' } 
+}
+
+if (hoge() && fuga()) {
+  console.log('ok')
+}
+
+console.log('end')
