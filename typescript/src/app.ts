567770e24e1b7d3066e340f660e87db6f5e54526
romukey <baymax.blackandwhite@gmail.com>
2019-12-29 21:07:54 +0900

    add generics

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -49,3 +49,38 @@ console.log(Add(3, 5))
 console.log(Substract(6, 2))
 console.log(Multiply(3))
 console.log(Divide(10))
+
+// generics
+interface Box<T> {
+  value: T
+}
+
+const box0: Box<string> = { value: 'test' }
+console.log({ box0 })
+
+interface Box1<T extends string | number> {
+  value: T
+}
+
+const box1: Box1<number> = { value: 100 }
+
+console.log({ box1 })
+
+const pick = <K extends keyof T, T>(key: K, props: T) => {
+  return props[key]
+}
+
+const obj = {
+  name: 'Taro',
+  amount: 0,
+  flag: false,
+}
+
+const value1 = pick('name', obj)
+console.log({ value1 })
+
+const value2 = pick('amount', obj)
+console.log({ value2 })
+
+const value3 = pick('flag', obj)
+console.log({ value3 })
